09e8793f5e41ced16d3fd0dab16e883c9710144c
probabilitydiht <probability.diht@yandex.ru>
2016-02-23 04:08:18 +0300

    Delete run.sh

--- sm3/map_side_join/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-yarn jar /opt/hadoop/hadoop-streaming.jar \
--files mapper.py,ip_region.txt \
--mapper "./mapper.py ip_region.txt" \
--input /user/shtokhov/logs \
--output /user/shtokhov/out2 \
